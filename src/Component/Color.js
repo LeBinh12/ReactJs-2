dea217d34ac7e7acae0e13c626a4482b8c9fe13f
LeBinh12 <lephuocbinh.2000@gmail.com>
2025-01-07 01:01:57 +0700

    Props type checking2

--- src/Component/Color.js
+++ src/Component/Color.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import PropTypes from 'prop-types';
 
 function ColorBox(props)
 {
@@ -8,5 +8,21 @@ function ColorBox(props)
         <div className='box' style={{backgroundColor: color}}></div>
     )
 }
+
+
+ColorBox.prototype = {
+    color: PropTypes.string.isRequired, // Định nghĩa này là bắt buộc bạn phải truyền tham số và vào
+
+    
+    // rounded: PropTypes.string, // bạn sử dụng rounded nếu bạn muốn khi không truyền tham số nào 
+    // thì mặt định nó màu đỏ mà bạn muốn hiển thị khi không truyền tham số
+}
+
+ColorBox.defaultProps = {
+    rounded: 'red', //
+}
+
+
+
 
 export default ColorBox;
