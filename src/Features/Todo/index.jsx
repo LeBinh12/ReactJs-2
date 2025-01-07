5124ff009d2a29ef660fe136fb808ba8a6c19ddf
LeBinh12 <lephuocbinh.2000@gmail.com>
2025-01-07 21:48:56 +0700

    Filter Todo

--- src/Features/Todo/index.jsx
+++ src/Features/Todo/index.jsx
@@ -32,6 +32,8 @@ function TodoFeature(props) {
 
     const [todoList, setTodoList] = useState(initTodoList); 
 
+    const [filteredStatus, setFilteredStatus] = useState('all');
+
     const handleTodoList = (todo, index) => {
         //taoj ra 1 mangx mowi
         const newTodoList = [...todoList];
@@ -50,10 +52,31 @@ function TodoFeature(props) {
         setTodoList(newTodoList);
     }
 
+    const handleShowAllClick = () => {
+        setFilteredStatus('all');
+    }
+    const handleShowCompletedClick = () => { 
+        setFilteredStatus('completed');
+    }
+    const handleShowNewClick = () => {
+        setFilteredStatus('new');
+    }
+
+    const renderedTodoList = todoList.filter(todo => filteredStatus === 'all'
+        || filteredStatus === todo.status);
+    
+    console.log(renderedTodoList);
+
     return (
         <div>
             <h3>Todo List</h3>
-            <TodoList todoList={todoList} onTodoClick={handleTodoList} />
+            <TodoList todoList={renderedTodoList} onTodoClick={handleTodoList} />
+
+
+            <button onClick={handleShowAllClick} >Show All</button>
+            <button onClick={handleShowCompletedClick} >Show Completed</button>
+            <button onClick={handleShowNewClick} >Show New</button>
+
         </div>
     );
 }
