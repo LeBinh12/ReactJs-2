51d8ae4071845ea6c1da2d7a6f2232fbca698303
LeBinh12 <lephuocbinh.2000@gmail.com>
2025-01-08 20:36:39 +0700

    Đồng bộ state lên URL

--- src/Features/Todo/Page/ListPage/index.jsx
+++ src/Features/Todo/Page/ListPage/index.jsx
@@ -1,12 +1,16 @@
-import React, { useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import PropTypes from 'prop-types';
 import TodoList from '../../Components/TodoList';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import queryString from 'query-string';
 
 ListPage.propTypes = {
     
 };
+
+
+// trong reachjs v6 chungs ta khoong cần sử dụng 'useHistory, useMatch'
+//  thay vào đó chúng ta có thể dùng useNavigate cho dễ dàng hơn
 
 function ListPage(props) {
     const initTodoList = [
@@ -32,6 +36,8 @@ function ListPage(props) {
 
     const location = useLocation();
 
+    const navigation = useNavigate();
+
     const [todoList, setTodoList] = useState(initTodoList); 
 
     const [filteredStatus, setFilteredStatus] = useState(() => {
@@ -39,6 +45,13 @@ function ListPage(props) {
         // console.log(params.status);
         return params.status || 'all';
     });
+
+
+    // lệnh này dùng để khi người dùng loadpage thì dữ liệu hiện tại trên page vẫn được giữ lại và nó không load
+    useEffect(() => {
+        const params = queryString.parse(location.search);
+        setFilteredStatus(params.status || 'all');
+    }, [location.search]);
 
     const handleTodoList = (todo, index) => {
         //taoj ra 1 mangx mowi
@@ -60,16 +73,39 @@ function ListPage(props) {
 
     const handleShowAllClick = () => {
         setFilteredStatus('all');
+
+        const queryParams = { status: 'all' };
+        
+        navigation({
+            path: '/',
+            search: queryString.stringify(queryParams)
+        })
     }
     const handleShowCompletedClick = () => { 
         setFilteredStatus('completed');
+
+        const queryParams = { status: 'completed' };
+        
+        navigation({
+            path: '/',
+            search: queryString.stringify(queryParams)
+        })
     }
     const handleShowNewClick = () => {
         setFilteredStatus('new');
-    }
 
-    const renderedTodoList = todoList.filter(todo => filteredStatus === 'all'
-        || filteredStatus === todo.status);
+        const queryParams = { status: 'new' };
+        
+        navigation({
+            path: '/',
+            search: queryString.stringify(queryParams)
+        })
+    }
+    // chức năng này chỉ thay đổi khi todoList hoặc filteredStatus thay đổi giá trị đầu vào
+    const renderedTodoList = useMemo(() => {
+        return todoList.filter(todo => filteredStatus === 'all'
+                        || filteredStatus === todo.status);
+    }, [todoList, filteredStatus]) 
     
     console.log(renderedTodoList);
 
