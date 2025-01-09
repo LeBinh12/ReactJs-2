02dea0b2de25d92bf385652093693ca815b29794
LeBinh12 <lephuocbinh.2000@gmail.com>
2025-01-10 00:43:43 +0700

    Material styles

--- src/Features/Couter/index.jsx
+++ src/Features/Couter/index.jsx
@@ -2,13 +2,28 @@ import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { decrease, increase } from './CouterSilce';
 import styles from './style.module.css';
+import { makeStyles } from '@mui/styles';
+import Button from '@mui/material/Button';
 
+const useStyles = makeStyles({
+  root: {
+    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+    border: 0,
+    borderRadius: 3,
+    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+    color: 'white',
+    height: 30,
+    padding: '0 30px',
+  },
+});
 
 CounterFeature.propTypes = {
     
 };
 
 function CounterFeature(props) {
+    const classes = useStyles();
+
     const counter = useSelector(state => state.count);
     const dispatch = useDispatch()
 
@@ -26,8 +41,8 @@ function CounterFeature(props) {
         <div className={styles.counter}>
             Count: {counter}
             <div>
-                <button onClick={handleIncreaseClick}>Add 1</button>
-                <button onClick={handleDecreaseClick}>Tru 1</button>
+                <Button className={classes.root} onClick={handleIncreaseClick}>Add 1</Button>
+                <Button className={classes.root} onClick={handleDecreaseClick}>Tru 1</Button>
             </div>
         </div>
     );
